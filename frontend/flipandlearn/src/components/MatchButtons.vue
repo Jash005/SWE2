6f129357cb1c6b068c660355ca8113d7da2d0280
Jasminh <mail@jasminh.de>
2025-04-13 12:09:29 +0200

    add visual-effect for matchbuttons to choice if is a match #SCRUM-39

--- frontend/flipandlearn/src/components/MatchButtons.vue
+++ frontend/flipandlearn/src/components/MatchButtons.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="match-buttons">
-    <button @click="$emit('decision', true)">Passt</button>
-    <button @click="$emit('decision', false)">Passt nicht</button>
+    <button @click="$emit('decision', true)">
+      <span class="material-symbols-outlined">check</span>
+      Passt
+    </button>
+    <button @click="$emit('decision', false)">
+      <span class="material-symbols-outlined">close</span>
+      Passt nicht
+    </button>
   </div>
 </template>
 
@@ -18,20 +24,52 @@ export default {
   gap: 10px;
   margin-bottom: 20px;
 }
+.match-buttons button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    font-size: 22px;
+    font-weight: 7000;
+  }
 
 button {
   padding: 10px 20px;
   font-size: 16px;
   font-weight: bold;
-  border: none;
+  border: 2px solid transparent;
   border-radius: 5px;
   cursor: pointer;
   background-color: var(--button);
   color: var(--button-font);
-  transition: background-color 0.3s ease;
+  animation: pulse 1.5s infinite; /* Pulsierende Animation */
+}
+
+@keyframes pulse {
+  0% {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    border-color: transparent;
+  }
+  50% {
+    box-shadow: 0 0 35px rgba(0, 0, 0, 0.85);
+    border-color: var(--button-hover);
+  }
+  100% {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    border-color: transparent;
+  }
 }
 
 button:hover {
   background-color: var(--button-hover);
+}
+
+@media screen and (min-width: 768px) {
+    .match-buttons {
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+    }
+
 }
 </style>
