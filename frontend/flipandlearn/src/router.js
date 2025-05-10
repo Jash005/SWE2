aa2af1a2adda2a3cc88ab9fc5b942ff28f8a313c
rinaraj <raj@matoma.de>
2025-05-10 11:14:49 +0200

    Add router guard

--- frontend/flipandlearn/src/router.js
+++ frontend/flipandlearn/src/router.js
@@ -7,25 +7,86 @@ import ProfilView from "./components/ProfilView.vue";
 import CardCreateView from "./components/CardCreateView.vue";
 import GameResult from "./components/GameResult.vue";
 import NotFound from "./components/NotFound.vue";
+import { useAuthStore } from "./stores/auth-store";
 
 const routes = [
-  { path: "/", name: "Home", component: HomeView },
-  { path: "/login", name: "Login", component: LoginView },
-  { path: "/game", name: "Game", component: GameView },
+  { 
+    path: "/", 
+    name: "Home", 
+    component: HomeView,
+    meta: { requiresAuth: false }, // This route does not require authentication 
+  },
+  { 
+    path: "/login", 
+    name: "Login", 
+    component: LoginView,
+    meta: { requiresAuth: false }
+  },
+  { 
+    path: "/game", 
+    name: "Game", 
+    component: GameView,
+    meta: { requiresAuth: true }, // This route requires the user to be logged in
+  },
   {
     path: "/card-management",
     name: "CardManagement",
     component: CardManagmentView,
+    meta: { requiresAuth: true },
   },
-  { path: "/profil", name: "Profil", component: ProfilView },
-  { path: "/card-create", name: "CardCreate", component: CardCreateView },
-  { path: "/game-result", name: "GameResult", component: GameResult },
-  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+  { 
+    path: "/profil", 
+    name: "Profil", 
+    component: ProfilView, 
+    meta: { requiresAuth: true },
+  },
+  { 
+    path: "/card-create", 
+    name: "CardCreate", 
+    component: CardCreateView,
+    meta: { requiresAuth: true },
+  },
+  { 
+    path: "/game-result", 
+    name: "GameResult", 
+    component: GameResult,
+    meta: { requiresAuth: true }
+  },
+  { 
+    path: "/:pathMatch(.*)*", 
+    name: "NotFound", 
+    component: NotFound,
+    meta: { requiresAuth: false }
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+
+// Route guard to check if user is logged in
+  router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore();
+
+    // Check if the route requires authentication
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+      
+      // If user is not logged in, redirect to login page
+      if (!authStore.loggedIn && !localStorage.getItem('user')) {
+
+        // TODO: Show notification to user about the need to log in
+        next('/login');
+      } 
+      else {
+        next(); // Proceed to the requested route
+      }
+    } 
+    else {
+      next(); // If the route does not require authentication, just proceed
+    }
+  });
+ 
 
 export default router;
